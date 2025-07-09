382a4eeb41aeb54e07f95fc9a477c3d23227c755
Léon Tshomba <leonttshomba@gmail.com>
2025-07-09 10:08:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World from VERCEL!"
+    
+@app.route("/api")
+def api():
+    return "API: Ni mimi uyo SASA!"
 
 if __name__ == "__main__":
     app.run()
